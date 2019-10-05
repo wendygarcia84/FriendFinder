459b7565e2de2bc5ff8af8509434c69d314db8d4
Wendy Garcia <wendy.fern84@gmail.com>
2019-10-04 20:28:46 -0700

    ready to deploy to heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var addHTMLRoutes = require("./app/routing/htmlRoutes");
 
 var app = express();
 
-var PORT = 8080;
+var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
